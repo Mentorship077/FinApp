a571f9c19897c90aefa7961e2e6702954b77c807
Nazar_Khimin <Nazar_Khimin@epam.com>
2018-05-16 18:20:53 +0300

    fix constant

--- src/main/java/com/epam/organizer/commons/LevelConst.java
+++ src/main/java/com/epam/organizer/commons/LevelConst.java
@@ -4,26 +4,21 @@ public class LevelConst {
 
 //    Junior
     public static int JUN_SALARY = 1000;
-    public static int JUN_OVERHEAD = 670;
-    public static int JUN_COST = 150;
+    public static int JUN_OVERHEAD = 30;
 
 //    Intermediate
     public static int INTER_SALARY = 2000;
-    public static int INTER_OVERHEAD = 670;
-    public static int INTER_COST = 250;
+    public static int INTER_OVERHEAD = 30;
 
 //    Senior
     public static int SENIOR_SALARY = 3000;
-    public static int SENIOR_OVERHEAD = 670;
-    public static int SENIOR_COST = 350;
+    public static int SENIOR_OVERHEAD = 30;
 
 //    Lead
     public static int LEAD_SALARY = 4000;
-    public static int LEAD_OVERHEAD = 670;
-    public static int LEAD_COST = 450;
+    public static int LEAD_OVERHEAD = 30;
 
 //    Above lead
     public static int ABOVE_LEAD_SALARY = 4500;
-    public static int ABOVE_LEAD_OVERHEAD = 670;
-    public static int ABOVE_LEAD_COST = 550;
+    public static int ABOVE_LEAD_OVERHEAD = 30;
 }
